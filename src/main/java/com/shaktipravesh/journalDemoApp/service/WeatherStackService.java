c7ea4a0393b6b48cff422a8df3dd395a2bf63f1a
shaktipravesh <shaktipravesh@yahoo.co.in>
2024-12-09 19:41:57 +0530

    pom file changes for sonar cloud

---
+++ src/main/java/com/shaktipravesh/journalDemoApp/service/WeatherStackService.java
@@ -0,0 +1,27 @@
+package com.shaktipravesh.journalDemoApp.service;
+
+import com.shaktipravesh.journalDemoApp.api.response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WeatherStackService {
+    private static final String APIKEY = "fafaa4355f160b9ea6f0a31aee2a0f36";
+    private static final String API_URL = "https://api.weatherstack.com/current?";
+
+    private final RestTemplate restTemplate;
+
+
+    @Autowired
+    public WeatherStackService(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    public WeatherResponse getWeather(String city) {
+        String finalAPI = API_URL + "access_key=" + APIKEY + "&query=" + city;
+        return restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class).getBody();
+    }
+}
